d06edc46b885e1a2c0af2044b22169da2d9a24bc
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-03-10 11:37:43 +0700

    versi1

---
+++ upload/readme.php
@@ -0,0 +1,9 @@
+<!--
+	Name        : Modul Ebook
+	Description : Sistem informasi buku elektronik
+	Date        : 2019
+	Developer   : Ardi Tri Heru
+	Phone/WA    : 0896-2967-1717
+	Email       : arditriheruh@gmail.com
+	Website     : https://www.arditriheru.com
+-->
